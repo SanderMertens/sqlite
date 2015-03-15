5c1407a9d301f3a76f89989f74e77968ced90d52
Johnny Lee <jleeothon@gmail.com>
2015-03-15 14:18:48 -0600

    #19 Made a static database connection object

--- src/sqlite_server.c
+++ src/sqlite_server.c
@@ -15,13 +15,14 @@
 #include <sqlite3.h>
 
 static const char filename[] = "db.sqlite";
+static sqlite3 *db;
 
-static cx_bool isBlacklisted(cx_object *o) {
+static cx_bool isBlacklisted(cx_object o) {
     cx_bool result = FALSE;
     if (o == root_o) {
         result = TRUE;
     } else if (cx_checkAttr(o, CX_ATTR_SCOPED)) {
-        cx_object *p = o;
+        cx_object p = o;
         while ((p = cx_parentof(p)) != root_o) {
             if (p == cortex_o) {
                 result = TRUE;
@@ -42,10 +43,6 @@ static cx_void bootstrapDatabase(void) {
         ");"
         ;
     char *errMsg = NULL;
-    sqlite3 *db;
-    if (sqlite3_open(filename, &db) != SQLITE_OK) {
-        cx_critical("%s", sqlite3_errmsg(db));
-    }
     if (sqlite3_exec(db, "PRAGMA foreign_keys = ON;", NULL, NULL, &errMsg) != SQLITE_OK) {
         cx_critical(errMsg);
         sqlite3_free(errMsg);
@@ -54,7 +51,6 @@ static cx_void bootstrapDatabase(void) {
         cx_critical(errMsg);
         sqlite3_free(errMsg);
     }
-    sqlite3_close(db);
 }
 /* $end */
 
@@ -63,6 +59,9 @@ cx_int16 sqlite_server_construct(sqlite_server _this) {
 /* $begin(::cortex::sqlite::server::construct) */
     if (!cx_fileTest(filename)) {
         cx_touch(filename);
+    }
+    if (sqlite3_open(filename, &db) != SQLITE_OK) {
+        cx_critical("%s", sqlite3_errmsg(db));
     }
     bootstrapDatabase();
     sqlite_server_onDeclare_o->mask = CX_ON_DECLARE | CX_ON_SCOPE | CX_ON_SELF;
@@ -79,6 +78,7 @@ cx_int16 sqlite_server_construct(sqlite_server _this) {
 cx_void sqlite_server_destruct(sqlite_server _this) {
 /* $begin(::cortex::sqlite::server::destruct) */
     CX_UNUSED(_this);
+    sqlite3_close(db);
 /* $end */
 }
 
@@ -89,10 +89,10 @@ cx_void sqlite_server_onDeclare(sqlite_server _this, cx_object *observable, cx_o
     CX_UNUSED(observable);
     char *errMsg = NULL;
     if (!isBlacklisted(source)) {
-        sqlite3 *db;
         struct cx_serializer_s serializer = cx_sqlite_declare_ser(CX_PRIVATE, CX_NOT, CX_SERIALIZER_TRACE_NEVER);
         cx_sqlite_ser_t sqlData = {NULL, NULL, 0, 0, 0};
         cx_serialize(&serializer, source, &sqlData);
+        sqlite3 *db;
         if (sqlite3_open(filename, &db) != SQLITE_OK) {
             cx_critical((char *)sqlite3_errmsg(db));
         }
@@ -109,6 +109,11 @@ cx_void sqlite_server_onDeclare(sqlite_server _this, cx_object *observable, cx_o
             cx_critical("error closing database");
         }
         cx_debug(sqlData.buffer);
+    } else if (cx_instanceof(cx_type(cx_type_o), source)) {
+        struct cx_serializer_s serializer = cx_type_ser(CX_PRIVATE, CX_NOT, CX_SERIALIZER_TRACE_NEVER);
+        cx_sqlite_ser_t sqlData = {NULL, NULL, 0, 0, 0};
+        cx_metaWalk(&serializer, cx_type(source), &sqlData);
+        cx_debug(sqlData.buffer);
     }
 /* $end */
 }
@@ -117,17 +122,17 @@ cx_void sqlite_server_onDeclare(sqlite_server _this, cx_object *observable, cx_o
 cx_void sqlite_server_onDefine(sqlite_server _this, cx_object *observable, cx_object *source) {
 /* $begin(::cortex::sqlite::server::onDefine) */
     CX_UNUSED(_this);
-    CX_UNUSED(observable);
-    if (!isBlacklisted(source)) {
-        if (cx_instanceof(cx_type(cx_type_o), source)) {
+    CX_UNUSED(source);
+    if (!isBlacklisted(observable)) {
+        if (cx_instanceof(cx_type(cx_type_o), observable)) {
             struct cx_serializer_s serializer = cx_type_ser(CX_PRIVATE, CX_NOT, CX_SERIALIZER_TRACE_NEVER);
             cx_sqlite_ser_t sqlData = {NULL, NULL, 0, 0, 0};
-            cx_metaWalk(&serializer, cx_type(source), &sqlData);
-            printf("%s\n", sqlData.buffer);
+            cx_metaWalk(&serializer, cx_type(observable), &sqlData);
+            printf("%s\n", sqlData.buffer); // TODO DELETE
         } else {
             struct cx_serializer_s serializer = cx_sqlite_define_ser(CX_PRIVATE, CX_NOT, CX_SERIALIZER_TRACE_NEVER);
             cx_sqlite_ser_t sqlData = {NULL, NULL, 0, 0, 0};
-            cx_serialize(&serializer, source, &sqlData);
+            cx_serialize(&serializer, observable, &sqlData);
             printf("%s\n", sqlData.buffer);
         }
     }
