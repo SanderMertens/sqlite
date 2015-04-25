0d2897eef1e77ad1e43fe0510f547d66eb53ef55
jleeothon <jleeothon@gmail.com>
2015-04-25 18:40:06 -0500

    #3 #6 Deserialize primitives and 1-level composites

--- src/sqlite_stmt_deser.c
+++ src/sqlite_stmt_deser.c
@@ -2,50 +2,94 @@
 #include <sqlite3.h>
 
 #include "cx.h"
+#include "cx_string.h"
 
-static int sqlite_stmt_deserPrimitive(sqlite3_stmt *stmt, cx_object o) {
-    CX_UNUSED(stmt);
-    CX_UNUSED(o);
-    return 0;
+
+static void complainCannotCoerceValue(char *text, cx_type type) {
+    cx_id typeFullname;
+    char *escapedString;
+    size_t length;
+    cx_fullname(type, typeFullname);
+    length = stresc(NULL, 0, text);
+    escapedString = cx_malloc(length + 1);
+    stresc(escapedString, length + 1, text);
+    cx_error("cannot coerce \"%s\" into %s", escapedString, typeFullname);
+    cx_dealloc(escapedString);
 }
 
-static void complainCannotCoerceValue(cx_object o, const char *columnName,
-        cx_type memberType, const char *columnText) {
-    cx_id fn, tfn, mtfn;
-    cx_fullname(o, fn);
-    cx_fullname(cx_typeof(o), tfn);
-    cx_fullname(memberType, mtfn);
-    cx_error("%s - cannot deserialize column %s from type %s; "
-        "\"%s\" cannot be coerced into %s",
-        fn, columnName, tfn, columnText, mtfn);
-}
+// static int sqlite_stmt_convertSignedInteger(void *to, cx_type type, char *text) {
+//     cx_int64 v = atol(text);
+//     if (v == 0) {
+//         cx_assert(!strcmp(text, "0"), "Not deserializing int value: %s", text);
+//     }
+//     int result = cx_convert(cx_primitive(cx_int32_o), &v, cx_primitive(type), &toValue);
+//     return result;
+// }
 
-static int setMember(void *o, cx_type type, char *text, char *columnName) {
-    void *toValue;
+// static int sqlite_stmt_convertUnsignedInteger(void *to, cx_type type, char *text) {
+//     cx_uint64 v = atoi(text);
+//     if (v == 0) {
+//         cx_assert(!strcmp(text, "0"), "Not deserializing int value: %s", text);
+//     }
+//     int result = cx_convert(cx_primitive(cx_int32_o), &v, cx_primitive(type), &toValue);
+//     return result;
+// }
+
+static int sqlite_stmt_setPrimitive(void *o, cx_type type, char *text) {
     int result = 0;
     switch (cx_primitive(type)->kind) {
+        case CX_ALIAS:
+            cx_error("alias primitive cannot be deserialized");
+            result = 1;
+            break;
         case CX_BINARY:
-        case CX_BITMASK:
-        case CX_BOOLEAN:
-        case CX_ENUM:
-        case CX_FLOAT:
         case CX_INTEGER:
+        case CX_FLOAT:
         case CX_UINTEGER:
+        case CX_CHARACTER:
+            result = cx_convert(cx_primitive(cx_string_o), &text, cx_primitive(type), o);
+            break;
+        case CX_BOOLEAN:
+        case CX_BITMASK:
+        case CX_ENUM:
             {
-                int v = atoi(text);
-                result = cx_convert(cx_primitive(cx_int32_o), &v, cx_primitive(type), &toValue);
+                cx_int32 v;
+                result = cx_convert(cx_primitive(cx_string_o), &text, cx_primitive(cx_int32_o), &v);
+                if (!result) {
+                    result = cx_convert(cx_primitive(cx_int32_o), &v, cx_primitive(type), o);
+                }
             }
             break;
         case CX_TEXT:
-            toValue = text;
+            *(char **)o = cx_strdup(text);
             break;
     }
-    if (result) {
-        complainCannotCoerceValue(o, columnName, type, text);
-    } else {
-        memcpy(o, &toValue, type->size);
-    }
+    // if (result) {
+    //     complainCannotCoerceValue(text, type);
+    // } else if (cx_primitive(type)->kind != CX_TEXT) {
+    //     memcpy(o, &toValue, type->size);
+    // } else {
+    // }
     return result;
+}
+
+static int sqlite_stmt_deserPrimitive(sqlite3_stmt *stmt, cx_object o) {
+    {
+        cx_id fullname;
+        int count = sqlite3_column_count(stmt);
+        char *columnName = (char *)sqlite3_column_name(stmt, 1);
+        cx_fullname(o, fullname);
+        cx_assert(count == 2, "did not find exactly 2 columns for %s (found %d)", fullname, count);
+        cx_assert(stricmp(columnName, "Value") == 0, "Column not Value for %s", fullname);
+    }
+    char *text = (char *)sqlite3_column_text(stmt, 1);
+    cx_debug("value found for %s: %s", cx_nameof(o), text);
+    if (sqlite_stmt_setPrimitive(o, cx_typeof(o), text)) {
+        goto error;
+    }
+    return 0;
+error:
+    return 1;
 }
 
 static int sqlite_stmt_deserComposite(sqlite3_stmt *stmt, cx_object o) {
@@ -69,7 +113,7 @@ static int sqlite_stmt_deserComposite(sqlite3_stmt *stmt, cx_object o) {
         {
             cx_type memberType = member->type;
             void *offset = CX_OFFSET(o, member->offset);
-            if (setMember(offset, memberType, columnText, columnName)) {
+            if (sqlite_stmt_setPrimitive(offset, memberType, columnText)) {
                 cx_error("cannot set member %s", columnName);
                 goto error;
             }
@@ -81,13 +125,33 @@ error:
 }
 
 int sqlite_stmt_deser(sqlite3_stmt *stmt, cx_object o) {
+    int result = 0;
+    {
+        int count = sqlite3_column_count(stmt);
+        cx_assert(count > 1, "Could not find at least 1 column");
+        char *objectId = (char *)sqlite3_column_name(stmt, 0);
+        cx_assert(stricmp(objectId, "ObjectId") == 0, "First column is not ObjectId");
+    }
     switch(cx_typeof(o)->kind) {
-        case CX_PRIMITIVE:
-            sqlite_stmt_deserPrimitive(stmt, o);
+        case CX_ANY:
+            cx_error("cannot deserialize void object");
+            result = 1;
             break;
+        case CX_COLLECTION:
+            cx_error("cannot deserialize collections");
+            break;
+        case CX_ITERATOR:
+            cx_error("cannot deserialize iterator");
         case CX_COMPOSITE:
-            sqlite_stmt_deserComposite(stmt, o);
+            result = sqlite_stmt_deserComposite(stmt, o);
+            break;
+        case CX_PRIMITIVE:
+            result = sqlite_stmt_deserPrimitive(stmt, o);
+            break;
+        case CX_VOID:
+            cx_error("cannot deserialize void object");
+            result = 1;
             break;
     }
-    return 0;
+    return result;
 }
